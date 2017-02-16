da1c08d48061578123cd6d218bba24a7443ef65f
yuhansun <wdmzssyh@gmail.com>
2017-02-15 22:13:37 -0700

    transfer table, query graph loader added

--- GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/Data_Loader.java
+++ GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/Data_Loader.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +19,7 @@ import org.neo4j.unsafe.batchinsert.BatchInserters;
 public class Data_Loader {
 	
 	//load graph without spatial info
-	public static void LoadGraph(String graphfile_path, String db_path)
+	public static void LoadGraph(String graphfile_path, String db_path, HashMap<Integer, Integer> transfer_table)
 	{
 		BufferedReader reader = null;
 		BatchInserter inserter = null;
@@ -59,7 +61,13 @@ public class Data_Loader {
 				}
 				int node_id = Integer.parseInt(line_list[1]);
 				int label = Integer.parseInt(line_list[2]);
-				Label node_label = DynamicLabel.label("GRAPH_"+label);
+				
+				Object object = transfer_table.get(label);
+				if(object == null)
+					throw new Exception("transfer table does not contain label!");
+				int transfer_label = (Integer)object;
+				
+				Label node_label = DynamicLabel.label("GRAPH_"+transfer_label);
 				inserter.createNode(node_id, null, node_label);
 			}
 			
@@ -79,15 +87,16 @@ public class Data_Loader {
 				}
 				int start = Integer.parseInt(line_list[1]);
 				int end = Integer.parseInt(line_list[2]);
-				inserter.createRelationship(start, end, edge_label, null);
+				if(start !=end)
+					inserter.createRelationship(start, end, edge_label, null);
 			}
 			reader.close();
 			inserter.shutdown();
 		}
 		catch(Exception e)
 		{
-			OwnMethods.Print(line);
 			e.printStackTrace();
+			OwnMethods.Print(line);
 		}
 		finally {
 			if(reader != null)
@@ -97,21 +106,78 @@ public class Data_Loader {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			if(inserter != null)
-				inserter.shutdown();
+		}
+	}
+	
+	public static void LoadQueryGraph(ArrayList<Query_Graph> query_Graphs, String db_path)
+	{
+		BatchInserter inserter = null;
+		Map<String, String> config = new HashMap<String, String>();
+		config.put("dbms.pagecache.memory", "10g");
+		
+		try
+		{
+			inserter = BatchInserters.inserter(new File(db_path).getAbsolutePath(),config);
+			for(int i = 0; i < query_Graphs.size(); i++)
+			{
+				Query_Graph query_Graph = query_Graphs.get(i);
+				int node_count = query_Graph.label_list.length;
+				int[] id_in_neo4j = new int[node_count];
+				int current_id = 0;
+				Label node_label = DynamicLabel.label("QUERY_"+i);
+				for(int label : query_Graph.label_list)
+				{
+					Map<String, Object> properties = new HashMap<String, Object>();
+					properties.put("id", current_id);
+					properties.put("label", label);
+					id_in_neo4j[current_id] = (int) inserter.createNode(properties, node_label);
+					current_id++;
+				}
+				
+				current_id = 0;
+				RelationshipType edge_label = DynamicRelationshipType.withName("LINK");
+				for(ArrayList<Integer> neighbors : query_Graph.graph)
+				{
+					for(int neighbor : neighbors)
+						if(current_id < neighbor)
+							inserter.createRelationship(id_in_neo4j[current_id], id_in_neo4j[neighbor], edge_label, null);
+					current_id++;
+				}
+			}
+			
+			inserter.shutdown();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
 		}
 	}
 
 	public static void LoadGraph()
 	{
-		String graphfile_path = "/home/yuhansun/Documents/GeoGraphMatchData/human";
-		String db_path = "/home/yuhansun/Documents/GeoGraphMatchData/neo4j-community-2.3.3_human/data/graph.db";
-		LoadGraph(graphfile_path, db_path);
+		String data_name = "hprd";
+		String graphfile_path = "/home/yuhansun/Documents/GeoGraphMatchData/" + data_name;
+		String db_path = "/home/yuhansun/Documents/GeoGraphMatchData/neo4j-community-2.3.3_"+data_name+"/data/graph.db";
+		String transfer_table_path = "/home/yuhansun/Documents/GeoGraphMatchData/transfertable_"+data_name+".txt";
+		HashMap<Integer, Integer> transfer_table = Utility.Read_Transfer_Table(transfer_table_path);
+		LoadGraph(graphfile_path, db_path, transfer_table);
+	}
+	
+	public static void LoadQueryGraph()
+	{
+		String data_name = "hprd";
+		String querygraph_path = "/home/yuhansun/Documents/GeoGraphMatchData/hprd25d";
+		String transfer_table_path = "/home/yuhansun/Documents/GeoGraphMatchData/transfertable_"+data_name+".txt";
+		HashMap<Integer, Integer> transfer_table = Utility.Read_Transfer_Table(transfer_table_path);
+		ArrayList<Query_Graph> query_Graphs  =  Utility.ReadQueryGraphs(querygraph_path, transfer_table);
+		
+		String db_path = "/home/yuhansun/Documents/GeoGraphMatchData/neo4j-community-2.3.3_"+data_name+"/data/graph.db";
+		LoadQueryGraph(query_Graphs, db_path);
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LoadGraph();
+//		LoadGraph();
+		LoadQueryGraph();
 	}
 
 }
