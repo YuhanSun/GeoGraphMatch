ae781ea23554aa35ad2935cb5ba04cf159eba66b
yuhansun <wdmzssyh@gmail.com>
2017-02-15 22:13:59 -0700

    supported java class

---
+++ GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/Label_Degree_Node.java
@@ -0,0 +1,14 @@
+package org.datasyslab.GeoGraphMatch;
+
+public class Label_Degree_Node {
+	public int id;
+	public int label;
+	public int degree;
+	
+	public Label_Degree_Node(int id, int label, int degree)
+	{
+		this.id = id;
+		this.label = label;
+		this.degree = degree;
+	}
+}
