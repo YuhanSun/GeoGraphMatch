bc229b46e5d0d597d5ec680c839d7f9a776f04d5
yuhansun <wdmzssyh@gmail.com>
2017-02-20 14:53:42 -0700

    supported java class

---
+++ GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/SearchUnit.java
@@ -0,0 +1,7 @@
+package org.datasyslab.GeoGraphMatch;
+
+public class SearchUnit {
+	int [] address;
+	int address_size;
+	int address_pos;
+}
---
+++ GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/SequenceUnit.java
@@ -0,0 +1,8 @@
+package org.datasyslab.GeoGraphMatch;
+
+public class SequenceUnit {
+	int node;
+	int parent_index;
+	int nte_length;
+	int start_pos;
+}
