0f588e01f059cd44e78b70bede37faed13159e6d
yuhansun <wdmzssyh@gmail.com>
2017-02-24 21:51:48 -0700

    spa_predicate added

--- GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/Query_Graph.java
+++ GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/Query_Graph.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 public class Query_Graph {
 	public int[] label_list;
 	ArrayList<ArrayList<Integer>> graph;
+	public MyRectangle[] spa_predicate;
 	
 	public Query_Graph(int node_count) {
 		label_list = new int[node_count];
 		graph = new ArrayList<ArrayList<Integer>>(node_count);
+		spa_predicate = new MyRectangle[node_count];
 	}
 }
