e092f50e8284b301c7aaa129d032f12e51001b6c
yuhansun <wdmzssyh@gmail.com>
2017-02-26 23:17:10 -0700

    rect corner added

--- GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/Config.java
+++ GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/Config.java
@@ -6,6 +6,11 @@ public class Config { public Config() {}
   private String longitude_property_name = "longitude";
   private String latitude_property_name = "latitude";
   
+  private String Rect_minx_name = "minx";
+  private String Rect_miny_name = "miny";
+  private String Rect_maxx_name = "maxx";
+  private String Rect_maxy_name = "maxy";
+  
   public String GetServerRoot() {
     return SERVER_ROOT_URI;
   }
@@ -16,5 +21,15 @@ public class Config { public Config() {}
   
   public String GetLatitudePropertyName() {
     return latitude_property_name;
+  }
+  
+  public String [] GetRectCornerName() 
+  {
+	  String [] rect_corner_name = new String[4];
+	  rect_corner_name[0] = this.Rect_minx_name;
+	  rect_corner_name[1] = this.Rect_miny_name;
+	  rect_corner_name[2] = this.Rect_maxx_name;
+	  rect_corner_name[3] = this.Rect_maxy_name;
+	  return rect_corner_name;
   }
 }
