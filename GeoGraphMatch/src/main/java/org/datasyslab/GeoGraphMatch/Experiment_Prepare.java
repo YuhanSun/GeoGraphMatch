0bdaa886ed74487f5285e3bbbec559d15b15c741
Yuhan Sun <wdmzssyh@gmail.com>
2017-05-11 23:10:27 -0700

    generate rectangle for query function consider number of spatial vertexes rather than rectangle size for spatial selectivity

--- GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/Experiment_Prepare.java
+++ GeoGraphMatch/src/main/java/org/datasyslab/GeoGraphMatch/Experiment_Prepare.java
@@ -79,60 +79,7 @@ public class Experiment_Prepare {
 		
 	}
 	
-	public static void GenerateQueryRectangle() {
-		int experiment_count = 50;
-		String entity_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/entity.txt", dataset);
-		ArrayList<Entity> entities = OwnMethods.ReadEntity((String)entity_path);
-		int spa_count = OwnMethods.GetSpatialEntityCount(entities);
-		STRtree stRtree = OwnMethods.ConstructSTRee(entities);
-		
-		String center_id_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/query/spa_predicate/"
-				+ "%s/%s_centerids.txt", dataset, dataset);
-		ArrayList<Integer> center_ids = OwnMethods.ReadCenterID(center_id_path);
-		ArrayList<Integer> final_center_ids = OwnMethods.GetRandom_NoDuplicate(center_ids, experiment_count);
-		
-		double selectivity = 0.000001;
-		int times = 10;
-		while (selectivity <= 1)
-		{
-			int k = (int) (selectivity * spa_count);
-			OwnMethods.Print(k);
-			String write_line = "";
-			for (int id : final_center_ids)
-			{
-				double lon = entities.get(id).lon;
-				double lat = entities.get(id).lat;
-				GeometryFactory factory = new GeometryFactory();
-				Point center = factory.createPoint(new Coordinate(lon, lat));
-				Object[] result = stRtree.kNearestNeighbour(center.getEnvelopeInternal(),
-						new GeometryFactory().toGeometry(center.getEnvelopeInternal()),
-						new GeometryItemDistance(), k);
-				double radius = 0.0;
-				for (Object object : result)
-				{
-					Point point = (Point) object;
-					double dist = center.distance(point);
-					if(dist > radius)
-						radius = dist;
-				}
-				OwnMethods.Print(radius);
-				double a = Math.sqrt(Math.PI) * radius;
-				double minx = center.getX() - a / 2;
-				double miny = center.getY() - a / 2;
-				double maxx = center.getX() + a / 2;
-				double maxy = center.getY() + a / 2;
-				
-				write_line += String.format("%s\t%s\t%s\t%s\n", String.valueOf(minx), String.valueOf(miny)
-						, String.valueOf(maxx), String.valueOf(maxy));
-			}
-			String output_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/"
-					+ "query/spa_predicate/%s/queryrect_%s.txt", dataset, String.valueOf(selectivity));
-			OwnMethods.WriteFile(output_path, true, write_line);
-			selectivity *= times;
-			
-		}
-	}
-	
+	//double as name will cause problem
 //	public static void GenerateQueryRectangle() {
 //		int experiment_count = 50;
 //		String entity_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/entity.txt", dataset);
@@ -145,12 +92,12 @@ public class Experiment_Prepare {
 //		ArrayList<Integer> center_ids = OwnMethods.ReadCenterID(center_id_path);
 //		ArrayList<Integer> final_center_ids = OwnMethods.GetRandom_NoDuplicate(center_ids, experiment_count);
 //		
-//		double base_selectivity = 0.005;
-//		int times = 1;
-//		while (times <= 8)
+//		double selectivity = 0.000001;
+//		int times = 10;
+//		while (selectivity <= 1)
 //		{
-//			double selectivity = base_selectivity * times;
 //			int k = (int) (selectivity * spa_count);
+//			OwnMethods.Print(k);
 //			String write_line = "";
 //			for (int id : final_center_ids)
 //			{
@@ -176,15 +123,71 @@ public class Experiment_Prepare {
 //				double maxx = center.getX() + a / 2;
 //				double maxy = center.getY() + a / 2;
 //				
-//				write_line += String.format("%f\t%f\t%f\t%f\n", minx, miny, maxx, maxy);
+//				write_line += String.format("%s\t%s\t%s\t%s\n", String.valueOf(minx), String.valueOf(miny)
+//						, String.valueOf(maxx), String.valueOf(maxy));
 //			}
 //			String output_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/"
-//					+ "query/spa_predicate/%s/queryrect_%d.txt", dataset, times);
+//					+ "query/spa_predicate/%s/queryrect_%s.txt", dataset, String.valueOf(selectivity));
 //			OwnMethods.WriteFile(output_path, true, write_line);
-//			times *= 2;
+//			selectivity *= times;
 //			
 //		}
 //	}
+	
+	/**
+	 * generate query rectangle with spatial vertices count as selectivity
+	 * rather than using size of rectangle
+	 */
+	public static void GenerateQueryRectangle() {
+		int experiment_count = 50;
+		String entity_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/entity.txt", dataset);
+		ArrayList<Entity> entities = OwnMethods.ReadEntity((String)entity_path);
+		int spa_count = OwnMethods.GetSpatialEntityCount(entities);
+		STRtree stRtree = OwnMethods.ConstructSTRee(entities);
+		
+		String center_id_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/query/spa_predicate/"
+				+ "%s/%s_centerids.txt", dataset, dataset);
+		ArrayList<Integer> center_ids = OwnMethods.ReadCenterID(center_id_path);
+		ArrayList<Integer> final_center_ids = OwnMethods.GetRandom_NoDuplicate(center_ids, experiment_count);
+		
+		double selectivity = 0.000001;
+		while (selectivity <= 0.1)
+		{
+			int name_suffix = (int) (selectivity * spa_count);
+			String write_line = "";
+			for (int id : final_center_ids)
+			{
+				double lon = entities.get(id).lon;
+				double lat = entities.get(id).lat;
+				GeometryFactory factory = new GeometryFactory();
+				Point center = factory.createPoint(new Coordinate(lon, lat));
+				Object[] result = stRtree.kNearestNeighbour(center.getEnvelopeInternal(),
+						new GeometryFactory().toGeometry(center.getEnvelopeInternal()),
+						new GeometryItemDistance(), name_suffix);
+				double radius = 0.0;
+				for (Object object : result)
+				{
+					Point point = (Point) object;
+					double dist = center.distance(point);
+					if(dist > radius)
+						radius = dist;
+				}
+				OwnMethods.Print(radius);
+				double a = Math.sqrt(Math.PI) * radius;
+				double minx = center.getX() - a / 2;
+				double miny = center.getY() - a / 2;
+				double maxx = center.getX() + a / 2;
+				double maxy = center.getY() + a / 2;
+				
+				write_line += String.format("%f\t%f\t%f\t%f\n", minx, miny, maxx, maxy);
+			}
+			String output_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/"
+					+ "query/spa_predicate/%s/queryrect_%d.txt", dataset, name_suffix);
+			OwnMethods.WriteFile(output_path, true, write_line);
+			selectivity *= 10;
+			
+		}
+	}
 
 	public static void GetRealSelectivity()
 	{
